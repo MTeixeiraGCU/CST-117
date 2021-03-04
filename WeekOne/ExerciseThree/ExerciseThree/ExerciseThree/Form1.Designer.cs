723fb0e85cbc1eca0e90d657fdf48849076d8f0b
Marc Teixeira <MTeixeira@my.gcu.edu>
2021-03-04 14:14:07 -0800

    Added Exercise 3

---
+++ WeekOne/ExerciseThree/ExerciseThree/ExerciseThree/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace ExerciseThree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEarthWeight = new System.Windows.Forms.Label();
+            this.lblMarsWeight = new System.Windows.Forms.Label();
+            this.txtEarthWeight = new System.Windows.Forms.TextBox();
+            this.txtMarsWeight = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEarthWeight
+            // 
+            this.lblEarthWeight.AutoSize = true;
+            this.lblEarthWeight.Location = new System.Drawing.Point(54, 41);
+            this.lblEarthWeight.Name = "lblEarthWeight";
+            this.lblEarthWeight.Size = new System.Drawing.Size(132, 13);
+            this.lblEarthWeight.TabIndex = 0;
+            this.lblEarthWeight.Text = "Enter your weight on Earth";
+            // 
+            // lblMarsWeight
+            // 
+            this.lblMarsWeight.AutoSize = true;
+            this.lblMarsWeight.Location = new System.Drawing.Point(82, 92);
+            this.lblMarsWeight.Name = "lblMarsWeight";
+            this.lblMarsWeight.Size = new System.Drawing.Size(104, 13);
+            this.lblMarsWeight.TabIndex = 1;
+            this.lblMarsWeight.Text = "Your weight on Mars";
+            // 
+            // txtEarthWeight
+            // 
+            this.txtEarthWeight.Location = new System.Drawing.Point(203, 38);
+            this.txtEarthWeight.Name = "txtEarthWeight";
+            this.txtEarthWeight.Size = new System.Drawing.Size(100, 20);
+            this.txtEarthWeight.TabIndex = 2;
+            this.txtEarthWeight.Text = "0";
+            // 
+            // txtMarsWeight
+            // 
+            this.txtMarsWeight.Location = new System.Drawing.Point(203, 89);
+            this.txtMarsWeight.Name = "txtMarsWeight";
+            this.txtMarsWeight.Size = new System.Drawing.Size(100, 20);
+            this.txtMarsWeight.TabIndex = 3;
+            this.txtMarsWeight.Text = "0";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(228, 138);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 216);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtMarsWeight);
+            this.Controls.Add(this.txtEarthWeight);
+            this.Controls.Add(this.lblMarsWeight);
+            this.Controls.Add(this.lblEarthWeight);
+            this.Name = "Form1";
+            this.Text = "Your Weight on Mars";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEarthWeight;
+        private System.Windows.Forms.Label lblMarsWeight;
+        private System.Windows.Forms.TextBox txtEarthWeight;
+        private System.Windows.Forms.TextBox txtMarsWeight;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ WeekOne/ExerciseThree/ExerciseThree/ExerciseThree/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciseThree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double earthGrav = 9.81;
+            double marsGrav = 3.711;
+
+            //grab earth weight
+            double earthWeight = double.Parse(txtEarthWeight.Text);
+
+            //generate mars weight
+            double marsWeight = (earthWeight / earthGrav) * marsGrav;
+
+            //display mars weight
+            txtMarsWeight.Text = marsWeight.ToString("f");
+        }
+    }
+}
