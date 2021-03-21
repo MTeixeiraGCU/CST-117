0680a223228a6721aba006f6d779338847e4527c
Marc Teixeira <MTeixeira@my.gcu.edu>
2021-03-21 12:56:24 -0700

    Added exercise 5

---
+++ WeekThree/ExerciseFive/ExerciseFive/ExerciseFive/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace ExerciseFive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTerms = new System.Windows.Forms.Label();
+            this.txtTerms = new System.Windows.Forms.TextBox();
+            this.lblEstimate = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTerms
+            // 
+            this.lblTerms.AutoSize = true;
+            this.lblTerms.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTerms.Location = new System.Drawing.Point(38, 50);
+            this.lblTerms.Name = "lblTerms";
+            this.lblTerms.Size = new System.Drawing.Size(177, 24);
+            this.lblTerms.TabIndex = 0;
+            this.lblTerms.Text = "Enter the # of terms:";
+            // 
+            // txtTerms
+            // 
+            this.txtTerms.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTerms.Location = new System.Drawing.Point(230, 50);
+            this.txtTerms.MaxLength = 9;
+            this.txtTerms.Name = "txtTerms";
+            this.txtTerms.Size = new System.Drawing.Size(113, 29);
+            this.txtTerms.TabIndex = 1;
+            this.txtTerms.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblEstimate
+            // 
+            this.lblEstimate.AutoSize = true;
+            this.lblEstimate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstimate.Location = new System.Drawing.Point(38, 173);
+            this.lblEstimate.Name = "lblEstimate";
+            this.lblEstimate.Size = new System.Drawing.Size(60, 24);
+            this.lblEstimate.TabIndex = 2;
+            this.lblEstimate.Text = "label1";
+            this.lblEstimate.Visible = false;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(38, 209);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(60, 24);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "label2";
+            this.lblAnswer.Visible = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(42, 99);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(121, 43);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(446, 263);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblEstimate);
+            this.Controls.Add(this.txtTerms);
+            this.Controls.Add(this.lblTerms);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Estimating PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTerms;
+        private System.Windows.Forms.TextBox txtTerms;
+        private System.Windows.Forms.Label lblEstimate;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ WeekThree/ExerciseFive/ExerciseFive/ExerciseFive/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciseFive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double pi = 0;
+            double divisor = 1;
+
+            //this variable is used to switch the add or subtract sign each iteration
+            bool add = true;
+
+            //reset display labels
+            lblEstimate.Hide();
+            lblAnswer.Hide();
+
+            //number of terms to calculate
+            int terms;
+            if(!int.TryParse(txtTerms.Text, out terms))
+            {
+                MessageBox.Show("You must enter a valid integer for the number of terms!");
+                return;
+            }
+            if(terms < 0)
+            {
+                MessageBox.Show("You must enter a positive value for terms!");
+                return;
+            }
+
+            //loop to estimate pi
+            for(int i = 0;i < terms;i++)
+            {
+                if(add)
+                {
+                    pi += (4 / divisor);
+                }
+                else
+                {
+                    pi -= (4 / divisor);
+                }
+
+                //setup for next iteration
+                add = !add;
+                divisor += 2;
+            }
+
+            //display results
+            lblEstimate.Text = "Approximate value of pi after " + terms.ToString() + " term(s)";
+            lblEstimate.Show();
+            lblAnswer.Text = "= " + pi.ToString();
+            lblAnswer.Show();
+        }
+    }
+}
