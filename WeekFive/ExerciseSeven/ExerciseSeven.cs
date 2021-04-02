c174a27388bca677b9172c4a81cb78cbad654bf1
Marc Teixeira <MTeixeira@my.gcu.edu>
2021-04-02 09:51:38 -0700

    Added exercise seven.

---
+++ WeekFive/ExerciseSeven/ExerciseSeven.cs
@@ -0,0 +1,37 @@
+namespace CST117Exercises
+{
+    class ExerciseSeven
+    {
+        //1.    Write a method that takes two integers and displays their sum.
+        public void PrintSum(int numOne, int numTwo) { }
+
+        //2.	Write a method that takes five doubles and returns their average.
+        public double Average(double numOne, double numTwo, double numThree, double numFour, double numFive) { }
+
+        //3.	Write a method that returns the sum of two randomly generated integers.
+        public int RandomSum() { }
+
+        //4.	Write a method that takes three integers and returns true if their sum is divisible by 3 and false otherwise.
+        public bool IsSumDivisableByThree(int numOne, int numTwo, int numThree) { }
+
+        //5.	Write a method that takes two strings and displays the string that has fewer characters.
+        public void PrintSmaller(string strOne, string strTwo) { }
+
+        //6.	Write a method that takes an array of doubles and returns the largest value in the array.
+        public double LargestValue(double[] arrOfDoubles) { }
+
+        //7.	Write a method that generates and returns an array of fifty integer values.
+        public int[] GenerateIntegerArray() { }
+
+        //8.	Write a method that takes two bool variables and return true if they have the same value and false otherwise.
+        public bool Equals(bool valueOne, bool valueTwo) { }
+
+        //9.	Write a method that takes an int and a double and returns their product.
+        public double Product(int intValue, double doubleValue) { }
+
+        //10.	Write a method that takes a two-dimensional array of integers and returns the average of the entries.
+        public double Average(int[,] numbers) { }
+
+    }
+
+}
